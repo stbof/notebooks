2955301a6d24dca33ccad211c9ce19ce7b0f162b
Stephanie Bodoff <stephanie.bodoff@databricks.com>
2018-04-04 13:41:52 -0700

    Update: .gitignore README.md dbutils.py flight.py

---
+++ dbutils.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+dbutils.help()
+
+# COMMAND ----------
+
+dbutils.fs.help()
+
+# COMMAND ----------
+
+dbutils.notebook.help()
+
+# COMMAND ----------
+
+dbutils.widgets.help()
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("dbfs:/databricks-datasets"))
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("dbfs:/"))
+
+# COMMAND ----------
+
